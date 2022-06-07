91cad18bf831f7135dae468c3926440aec17bb9a
ULTIMATE-Mystery <quanghayhay1801@gmail.com>
2022-06-08 06:37:17 +0700

    Add admin pages

---
+++ View/admin/pages/home.php
@@ -0,0 +1,98 @@
+<div class="content-wrapper" style="min-height: 365px; background-color: rgb(227, 227, 242);">
+	<div class="content-header">
+		<div class="container-fluid">
+			<div class="row mb-2">
+				<div class="col-sm-6">
+					<h1 class="m-0 text-dark">Home Page</h1>
+				</div><!-- /.col -->
+				<div class="col-sm-6">
+					<ol class="breadcrumb float-sm-right">
+						<li class="breadcrumb-item"><a href="#">Home</a></li>
+						<li class="breadcrumb-item active">HCMG Admin</li>
+					</ol>
+				</div>
+			</div>
+		</div>
+	</div>
+	<section class="content">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-lg-3 col-6">
+					<!-- small box -->
+					<div class="small-box bg-info">
+						<div class="inner">
+							<h3>7</h3>
+
+							<h4>Orders</h4>
+						</div>
+						<div class="icon">
+							<i class="ion ion-bag"></i>
+						</div>
+						<a href="#" class="small-box-footer">Details   <i class="fas fa-arrow-circle-right"></i></a>
+					</div>
+				</div>
+				<!-- ./col -->
+				<div class="col-lg-3 col-6">
+					<!-- small box -->
+					<div class="small-box bg-success">
+						<div class="inner">
+							<h3>77<sup style="font-size: 20px">%</sup></h3>
+
+							<h4>Statistics</h4>
+						</div>
+						<div class="icon">
+							<i class="ion ion-stats-bars"></i>
+						</div>
+						<a href="#" class="small-box-footer">Details   <i class="fas fa-arrow-circle-right"></i></a>
+					</div>
+				</div>
+				<!-- ./col -->
+				<div class="col-lg-3 col-6">
+					<!-- small box -->
+					<div class="small-box bg-primary">
+						<div class="inner">
+							<h3>7</h3>
+
+							<h4>Contacts</h4>
+						</div>
+						<div class="icon">
+							<i class="ion ion-chatbubble-working"></i>
+						</div>
+						<a href="#" class="small-box-footer">Details   <i class="fas fa-arrow-circle-right"></i></a>
+					</div>
+				</div>
+				<!-- ./col -->
+				<div class="col-lg-3 col-6">
+					<!-- small box -->
+					<div class="small-box bg-warning">
+						<div class="inner">
+							<h3>777</h3>
+
+							<h4 style="color: #fff">Customers</h4>
+						</div>
+						<div class="icon">
+							<i class="ion ion-person"></i>
+						</div>
+						<a href="#" class="small-box-footer">Details   <i class="fas fa-arrow-circle-right"></i></a>
+					</div>
+				</div>
+				<!-- ./col -->
+				<div class="col-lg-3 col-6">
+					<!-- small box -->
+					<div class="small-box bg-danger">
+						<div class="inner">
+							<h3>65</h3>
+
+							<h4>Ratings</h4>
+						</div>
+						<div class="icon">
+							<i class="ion ion-pie-graph"></i>
+						</div>
+						<a href="#" class="small-box-footer">Details   <i class="fas fa-arrow-circle-right"></i></a>
+					</div>
+				</div>
+				<!-- ./col -->
+			</div>
+		</div>
+	</section>
+</div>
