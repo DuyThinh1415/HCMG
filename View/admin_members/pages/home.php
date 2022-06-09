135112fb90be431c15ea40dd7baf8e5a8b521aae
ULTIMATE-Mystery <quanghayhay1801@gmail.com>
2022-06-09 18:58:29 +0700

    admin

--- View/admin_members/pages/home.php
+++ View/admin_members/pages/home.php
@@ -1,14 +1,14 @@
-<div class="content-wrapper" style="min-height: 1203.6px;">
+<div class="content-wrapper" style="min-height: 1203.6px; background-color: rgb(227, 227, 242)">
     <section class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Danh sách chuyên mục</h1>
+                    <h1>Admin Members</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Danh sách chuyên mục</li>
+                        <li class="breadcrumb-item"><a href="../../View/Admin_home">Home</a></li>
+                        <li class="breadcrumb-item active">Admin Members</li>
                     </ol>
                 </div>
             </div>
@@ -29,8 +29,8 @@
                                 <table id="example2" class="table table-bordered table-hover dataTable" role="grid" aria-describedby="example2_info">
                                     <thead>
                                         <tr role="row">
-                                            <th class="sorting_asc" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending">STT</th>
-                                            <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending">Tên chuyên mục</th>
+                                            <th class="sorting_asc" style="text-align: center;" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending">STT</th>
+                                            <th class="sorting" style="text-align: center;" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending">Admin name</th>
                                             <th style="text-align: center;" class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Sửa</th>
                                             <th style="text-align: center;" class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending">Xóa</th>
 
@@ -39,8 +39,8 @@
                                     <tbody>
 
                                         <tr role="row" class="odd">
-                                            <td class="sorting_1">1</td>
-                                            <td>Đời sống</td>
+                                            <td class="sorting_1" style="text-align: center;">1</td>
+                                            <td style="text-align: center;">Phạm Duy Quang</td>
                                             <td style="text-align: center;">
                                                 <span class="badge bg-primary">
                                                             <ion-icon name="create-outline"></ion-icon>
@@ -57,10 +57,10 @@
                                     </tbody>
                                     <tfoot>
                                         <tr>
-                                            <th rowspan="1" colspan="1">STT</th>
-                                            <th rowspan="1" colspan="1">Tên chuyên mục</th>
-                                            <th rowspan="1" colspan="1">Sửa</th>
-                                            <th rowspan="1" colspan="1">Xóa</th>
+                                            <th rowspan="1" colspan="1" style="text-align: center;">STT</th>
+                                            <th rowspan="1" colspan="1" style="text-align: center;">Admin name</th>
+                                            <th rowspan="1" colspan="1" style="text-align: center;">Sửa</th>
+                                            <th rowspan="1" colspan="1" style="text-align: center;">Xóa</th>
                                         </tr>
                                     </tfoot>
                                 </table>
